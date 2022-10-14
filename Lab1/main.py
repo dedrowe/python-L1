b5bcdc8cf6e4abdd5dc18c13b14a33cce6bb9841
Vlad Barishnikov <barushnikovvs@gmail.com>
2022-10-14 19:25:13 +0400

    исправил недочеты

--- Lab1/main.py
+++ Lab1/main.py
@@ -1,34 +1,44 @@
 import csv
 import requests
 import json
+import logging
 
 
-def file_input(value, data):
+def write_value_to_file(value: tuple) -> None:
     """
     Функция записывает данные в таблицу
+
+    value: строка которая будет записана в таблицу
     """
-    writer = csv.writer(data, dialect='unix')
-    writer.writerow(value)
+    try:
+        with open("dataset.csv", 'a') as dataset:
+            writer = csv.writer(dataset, dialect='unix')
+            writer.writerow(value)
+    except OSError:
+        logging.warning("Ошибка открытия файла")
 
 
-def take_value(source, temp_dataset):
+def parse_value(valute: str, min_date: str = '1992-07-01', source: str = 'https://www.cbr-xml-daily.ru/daily_json.js'):
     """
-    Функция получает данные с сайта до даты 2020-02-08(это самая последняя запись)
+    Функция получает данные с сайта до последней записи
+
+    valute: валюта, значение которой программа будет извлекать
+    min_date: дата последней записи
+    source: ссылка на запись
     """
     page_code = requests.get(source)
     d_page_code = json.loads(page_code.text)
 
     while True:
         page_code = requests.get("https:" + d_page_code['PreviousURL'])
-        file_input((d_page_code['Date'][:10], d_page_code["Valute"]['USD']['Value']), temp_dataset)
+        write_value_to_file((d_page_code['Date'][:10], d_page_code["Valute"][valute]['Value']))
         d_page_code = json.loads(page_code.text)
-        if d_page_code['Date'][:10] == "2020-02-08":
-            file_input((d_page_code['Date'][:10], d_page_code['Valute']['USD']['Value']), temp_dataset)
+        if d_page_code['Date'][:10] == min_date:
+            write_value_to_file((d_page_code['Date'][:10], d_page_code['Valute'][valute]['Value']))
             break
 
 
 if __name__ == "__main__":
-    dataset = open("dataset.csv", 'a')
-    url = 'https://www.cbr-xml-daily.ru/daily_json.js'
-    take_value(url, dataset)
-    dataset.close()
+    required_valute = 'USD'
+
+    parse_value(required_valute)
