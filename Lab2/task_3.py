497bb96325d6dffce26baba189350534098004fe
Vlad Barishnikov <barushnikovvs@gmail.com>
2022-11-13 01:32:49 +0400

    переделал перебор записей

--- Lab2/task_3.py
+++ Lab2/task_3.py
@@ -18,31 +18,25 @@ def write_data_to_file(data: list, destination: str) -> None:
         logging.warning('Ошибка открытия файла')
 
 
-def parse_data(data: str) -> list:
-    date = data.split(sep='-')
-    return date
-
-
-def create_list(source: str = 'dataset.csv', last_row: str = "1992-07-01, 125.26"):
+def create_list(source: str = 'dataset.csv', last_date: str = "1992-07-01"):
     with open(source, 'r') as dataset:
         reader = csv.reader(dataset, dialect='unix')
-        row = next(reader)[0]
-        rows = [row]
-        while row != last_row:
-            temp1 = parse_data(row.split(sep=', ')[0])
-            logging.info(f'Программа сейчас на дате: {row.split(sep=", ")[0]}, '
-                         f'последняя дата: {last_row.split(sep=", ")[0]}')
-            row = next(reader)[0]
-            temp2 = parse_data(row.split(sep=', ')[0])
+        rows = [next(reader)]
+        for row in reader:
+            temp1 = rows[-1][0].split(sep='-')
+            logging.info(f'Программа сейчас на дате: {row[0]}, '
+                         f'последняя дата: {last_date}')
+            temp2 = row[0].split(sep='-')
             if datetime.date(int(temp1[0]), int(temp1[1]), int(temp1[2])).isocalendar()[1] == \
                     datetime.date(int(temp2[0]), int(temp2[1]), int(temp2[2])).isocalendar()[1]:
                 rows.append(row)
             else:
-                path =\
-                    f'task_3/{rows[-1][:4]}{rows[-1][5:7]}{rows[-1][8:10]}_{rows[0][:4]}{rows[0][5:7]}{rows[0][8:10]}'
+                path = f'task_3/{rows[-1][0][:4]}{rows[-1][0][5:7]}{rows[-1][0][8:10]}' \
+                       f'_{rows[0][0][:4]}{rows[0][0][5:7]}{rows[0][0][8:10]}'
                 write_data_to_file(rows, path)
                 rows = [row]
-        path = f'task_3/{rows[-1][:4]}{rows[-1][5:7]}{rows[-1][8:10]}_{rows[0][:4]}{rows[0][5:7]}{rows[0][8:10]}'
+        path = f'task_3/{rows[-1][0][:4]}{rows[-1][0][5:7]}{rows[-1][0][8:10]}' \
+               f'_{rows[0][0][:4]}{rows[0][0][5:7]}{rows[0][0][8:10]}'
         write_data_to_file(rows, path)
 
 
